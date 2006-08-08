6287e9f718ea150297cc3c5dc63a4c2e6292900a
Alberto Alonso <rydencillo@gmail.com>
2006-08-08 19:06:00 +0000

    * 08/08/2006 - rydencillo@gmail.com   ----------------------------   Agregado el idioma castellano

--- modules/ozipper/src/main.cpp
+++ modules/ozipper/src/main.cpp
@@ -1,12 +1,13 @@
 #include "exception.h"
 #include "languagetpl.h"
 #include <fstream>
+#include <iostream>
 
 int main(int argc, char *argv[], char *env[])
 {
   try
   {
-    char tmp[512];
+/*    char tmp[512];
     std::string out;
     std::ifstream file;
     size_t bytes;
@@ -20,8 +21,18 @@ int main(int argc, char *argv[], char *env[])
     } while (bytes);
     file.close();
     
-    LanguageTemplate tpl("en");
-    tpl.Parse(out);
+    LanguageTemplate tpl("es");
+    tpl.Parse(out);*/
+    char tmp[2048];
+    std::cin.read(tmp, 2047);
+    
+    printf("Content-type: text/html; charset=UTF-8\r\n\r\n");
+    for (int i = 0; env[i]; i++)
+    {
+      printf("%s\n", env[i]);
+    }
+    printf("POST: %s\n", tmp);
+    printf("Test: %s\n", argv[1]);
   }
   catch (Exception &e)
   {
