db247214d165b155bf48cea104adbf104d5b5242
Alberto Alonso <rydencillo@gmail.com>
2006-12-03 07:01:42 +0000

    Eliminamos debug node.js

--- public_html/index.php
+++ public_html/index.php
@@ -44,7 +44,6 @@
 
     <title><?=T('OGame zipper')?></title>
     <link rel="stylesheet" type="text/css" href="style.css" />
-    <script type="text/javascript" src="node.js"></script>
     <script type="text/javascript" src="htmlspecialchars.js"></script>
     <script type="text/javascript" src="wysiwyg.js"></script>
     <script type="text/javascript" src="ajax.js"></script>
